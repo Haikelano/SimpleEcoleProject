c4d4001c671818ad3243ae5668cf405fccc2b4dc
Haykel Maaoui <haykel.maaoui@gmail.com>
2023-06-14 00:33:22 +0200

    Fix post api

--- src/main/java/com/bezkoder/spring/mongodb/reactive/model/Note.java
+++ src/main/java/com/bezkoder/spring/mongodb/reactive/model/Note.java
@@ -2,12 +2,14 @@ package com.bezkoder.spring.mongodb.reactive.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
  * Created by @hmaaoui
  **/
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Note {
